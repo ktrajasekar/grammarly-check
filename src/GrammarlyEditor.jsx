f8b9610e5264ed34e12feb7dd7e8dae5138ba9f6
Rajasekar T <kt.rajasekar@gmail.com>
2022-10-01 23:36:21 +0530

    Grammar Check API

---
+++ src/GrammarlyEditor.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { GrammarlyEditorPlugin } from '@grammarly/editor-sdk-react'
+
+ const GrammarlyEditor = () => {
+  return (
+
+    <GrammarlyEditorPlugin clientId="client_DtHSUugRBC1fH8RrSbNuxm">
+      <textarea />
+    </GrammarlyEditorPlugin>
+  )
+}
+
+
+export default GrammarlyEditor;
